0754cdcd642bd5dfd098287313b6b17ee1bebfd9
Vitali K <vitali.kukyan@gmail.com>
2018-12-04 13:17:21 +0100

    Demo 5
    Demo 5

---
+++ Allfiles/Mod05/Democode/New fetchWeb/New fetchWeb/indexScript.js
@@ -0,0 +1,47 @@
+﻿const personApiUrl = "https://randomuser.me/api/?results=3";
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+async function getPersons() {
+    try {
+
+        let responce = await fetch(personApiUrl);
+
+        if (responce.ok) {
+            const data = await responce.json();
+            const table = document.getElementById("PersonTable");
+
+            data.results.map(function (auther) {
+                const tr = createNode('tr'),
+                    fullNameRow = createNode('td'),
+                    PicRow = createNode('td'),
+                    PicEl = createNode('img'),
+                    emailRow = createNode('td');
+
+                fullNameRow.innerHtml = '${auther.title}. ${auther.name.last} ${auther.name.first}';
+                PicEl.src = auther.picture.medium;
+                emailRow.innerHtml = auther.email;
+
+                append(tr, fullNameRow);
+                append(PicRow, PicEl);
+                append(tr, PicRow);
+                append(tr, emailRow);
+                append(table, tr);
+            });
+        }
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    document.getElementById('addPersonsBtn').addEventListener('click', getPersons);
+});
+
